e7285ef89b1dc6d435d7b13965619c39ee9c6007
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-05-24 11:51:51 +0300

    fix indentation

--- src/test_app.py
+++ src/test_app.py
@@ -2,13 +2,9 @@ import pytest
 from unittest.mock import patch
 import os
 
-
 os.environ.setdefault('DB_CONNECTION', 'sqlite:///:memory:')
 
-
 from app import app, db
-
-
 
 
 @pytest.fixture
@@ -16,21 +12,16 @@ def client():
    app.config['TESTING'] = True
    client = app.test_client()
 
-
    # Setup the database
    with app.app_context():
        db.create_all()
 
-
    yield client
-
 
    # Teardown the database
    with app.app_context():
        db.session.remove()
        db.drop_all()
-
-
 
 
 def test_home_page(client):
@@ -45,15 +36,11 @@ def test_home_page(client):
    assert b'Page reload count: 2' in second_response.data
 
 
-
-
 def test_docker_logo(client):
    """Test the docker logo endpoint."""
    response = client.get('/logo')
    assert response.status_code == 200
    assert response.content_type == 'image/png'
-
-
 
 
 def test_health_check(client):
@@ -63,15 +50,11 @@ def test_health_check(client):
    assert response.data == b'Healthy'
 
 
-
-
 def test_readiness_check(client):
    """Test the readiness check."""
    with patch('app.time.time', return_value=0):
        response = client.get('/ready')
        assert response.status_code == 503
-
-
 
 
 def test_external_call(client):
@@ -82,4 +65,3 @@ def test_external_call(client):
        response = client.get('/external-call')
        assert b'Extarnal call response: Success' in response.data
        assert response.status_code == 200
-
